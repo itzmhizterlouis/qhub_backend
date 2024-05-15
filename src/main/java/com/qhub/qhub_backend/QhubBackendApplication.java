ee57c6b0b5471a884e3d3046a59dcf08243e7e25
Marvellous <itzmhizterlouis@gmail.com>
2024-05-15 19:42:53 +0100

    CORS Fix

--- src/main/java/com/qhub/qhub_backend/QhubBackendApplication.java
+++ src/main/java/com/qhub/qhub_backend/QhubBackendApplication.java
@@ -2,12 +2,28 @@ package com.qhub.qhub_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class QhubBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QhubBackendApplication.class, args);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
